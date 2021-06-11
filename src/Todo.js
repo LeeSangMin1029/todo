d7cc49cba4f78eccd8d31a94b0f1ef67155160c7
LeeSangMin1029 <sangmins39@gmail.com>
2021-06-11 18:47:55 +0900

    컴포넌트 세분화

--- src/Todo.js
+++ src/Todo.js
@@ -1,6 +1,19 @@
+import React from "react";
+import { getNowDate } from "./util/date.js";
+// style
+import { List } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 // 간단하게 todo 내용을 출력하는 모듈
-const Todo = ({ value }) => {
-  return <li>{value}</li>;
+const Todo = ({ todo }) => {
+  return (
+    <List.Item>
+      <List.Icon name="github" size="large" verticalAlign="middle" />
+      <List.Content>
+        <List.Header as="a">{todo.body}</List.Header>
+        <List.Description as="a">{getNowDate(todo.createdAt)}</List.Description>
+      </List.Content>
+    </List.Item>
+  );
 };
 
 export default Todo;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,23 +1,16 @@
 import React from "react";
-import { getNowDate } from "./util/date.js";
+import Todo from "./Todo.js";
 
 // style
 import { List } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 // 목록을 보여주는 컴포넌트
 const TodoList = ({ todos, onClickDelete }) => {
-  const list = todos.map((todo) => (
-    <List.Item onClick={onClickDelete} key={todo.id}>
-      <List.Icon name="github" size="large" verticalAlign="middle" />
-      <List.Content>
-        <List.Header as="a">{todo.body}</List.Header>
-        <List.Description as="a">{getNowDate(todo.createdAt)}</List.Description>
-      </List.Content>
-    </List.Item>
-  ));
   return (
     <List divided relaxed>
-      {list}
+      {todos.map((todo) => (
+        <Todo onClick={onClickDelete} key={todo.id} todo={todo} />
+      ))}
     </List>
   );
 };
