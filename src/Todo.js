dfee884f2db56a70a442f15d47f3bd922ee9ef3a
LeeSangMin1029 <sangmins39@gmail.com>
2021-06-11 18:51:31 +0900

    삭제하는 버튼 추가 및 useContext를 사용해서 이벤트 할당

--- src/Todo.js
+++ src/Todo.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import { getNowDate } from "./util/date.js";
+import EventContext from "./hook/useEventContext.js";
 // style
+import IconButton from "./style/IconButton.js";
 import { List } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 // 간단하게 todo 내용을 출력하는 모듈
 const Todo = ({ todo }) => {
+  const deleteEvent = useContext(EventContext);
   return (
     <List.Item>
-      <List.Icon name="github" size="large" verticalAlign="middle" />
       <List.Content>
         <List.Header as="a">{todo.body}</List.Header>
         <List.Description as="a">{getNowDate(todo.createdAt)}</List.Description>
       </List.Content>
+      <IconButton onClick={deleteEvent}>
+        Delete
+        <i className="close icon"></i>
+      </IconButton>
     </List.Item>
   );
 };
