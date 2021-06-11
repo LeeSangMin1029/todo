853dc5b727273fdf4b4f36eb4d6e4081052d4b22
LeeSangMin1029 <sangmins39@gmail.com>
2021-06-11 19:36:10 +0900

    컴포넌트 스타일에서 일반 css 스타일로 적용

--- src/Todo.js
+++ src/Todo.js
@@ -1,24 +1,30 @@
 import React, { useContext } from "react";
 import { getNowDate } from "./util/date.js";
 import EventContext from "./hook/useEventContext.js";
+
 // style
 import IconButton from "./style/IconButton.js";
-import { List } from "semantic-ui-react";
-import "semantic-ui-css/semantic.min.css";
+import styled from "styled-components";
+
+const Header = styled.div`
+  font-size: 1.5em;
+  padding: 10px 0px 10px 0px;
+`;
+
 // 간단하게 todo 내용을 출력하는 모듈
 const Todo = ({ todo }) => {
   const deleteEvent = useContext(EventContext);
   return (
-    <List.Item>
-      <List.Content>
-        <List.Header as="a">{todo.body}</List.Header>
-        <List.Description as="a">{getNowDate(todo.createdAt)}</List.Description>
-      </List.Content>
+    <div className="item">
+      <div className="content">
+        <Header>{todo.body}</Header>
+        <div className="description">{getNowDate(todo.createdAt)}</div>
+      </div>
       <IconButton onClick={deleteEvent}>
         Delete
         <i className="close icon"></i>
       </IconButton>
-    </List.Item>
+    </div>
   );
 };
 
