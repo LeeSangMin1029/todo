93b6efd9c2a0620d5c4254901be176f25456e45c
LeeSangMin1029 <sangmins39@gmail.com>
2021-06-12 20:01:33 +0900

    context 추가

--- src/hook/useEventContext.js
+++ src/hook/useEventContext.js
@@ -1,9 +1,15 @@
-import { createContext } from "react";
+import { createContext, useContext } from 'react';
 
 const EventContext = createContext({
   onClickDelete: (id) => {},
+  onChangeTodo: (id) => {},
 });
 
 export const EventProvider = EventContext.Provider;
-
+export const useEventContext = () => {
+  const context = useContext(EventContext);
+  if (!context) {
+    throw new Error('Cannot find EventProvider');
+  }
+};
 export default EventContext;
